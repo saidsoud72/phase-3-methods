6c4728aeb06d90da7d583ef6945bbc74d83010fd
saidsoud72 <said.omar@student.moringaschool.com>
2022-10-19 19:45:00 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+#greet_programmer
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+
+#greet
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+#greet_with_default
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+#add
+def add(num1, num2)
+    return num1+num2
+end
+
+#halve
+def halve(num)
+    if num.class!=Integer
+        return nil
+    end
+    num/2
+end
